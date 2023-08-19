4630cfdf63161b2185289c43117bcb254a9d7719
guill <guillaume.michel@epitech.eu>
2023-08-19 16:21:18 +0200

    Add routing

---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import HomePage from './pages/index';
+import AboutPage from './pages/about';
+
+const routes = [
+    { path: '/', page: HomePage },
+    { path: '/about', page: AboutPage },
+    // Add more routes as needed
+];
+
+export default routes;
