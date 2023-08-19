890c86d3d245b7f394a00200afe5ff3403a7de1a
titanfrigel <kling.gaetan@gmail.com>
2023-08-19 21:43:10 +0200

    [Add] Shiba + Text + Shadows

---
+++ src/pages/LightAndShiba.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stats } from "@react-three/drei";
+
+import { Shiba } from '@/components/shiba';
+import { Text3D } from '@/components/Text3D';
+import { PlaneShadow } from '@/components/PlaneShadow';
+
+const Scene = () => {
+    return (
+        <group>
+            <Text3D text={"Website"} />
+            <Shiba />
+            <PlaneShadow />
+        </group>
+    );
+};
+
+const LightAndShibaPage = () => {
+    return (
+        <div>
+            <div className='flex justify-center items-center' style={{height: "100vh", width: "100vw"}}>
+                <Canvas shadows={"basic"} style={{backgroundColor: "black"}}>
+                    <pointLight position={[0, 2, 20]} intensity={10000} castShadow={true }/>
+
+                    <Scene />
+
+                    <OrbitControls />
+                    <Stats />
+                </Canvas>
+            </div>
+        </div>
+    );
+};
+
+export default LightAndShibaPage;
