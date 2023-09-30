e49d147cbc7e919b893c886af2f13f8954771c30
titanfrigel <kling.gaetan@gmail.com>
2023-09-30 23:07:43 +0200

    [Fix] Forgot menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import HomePage from "@/pages/HomePage";
+import Header from "@/components/Header/Header";
 export default function index() {
     return (
-        <HomePage />
+        <>
+            <Header />
+            <HomePage />
+        </>
     );
 };
